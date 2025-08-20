707bcb11c04646189e4d22978378a9096bac4980
Gustavo Nunes Melo <gustavonunesmelo@outlook.com>
2025-08-19 22:43:49 -0300

    Create selection_sort_otimizado.py

---
+++ atividade2/selection_sort_otimizado.py
@@ -0,0 +1,18 @@
+def selection_sort_otimizado(lista):
+    iteracoes = 0
+    for i in range(len(lista) - 1):
+        pos_menor = i
+        houve_troca = False
+        for j in range(i + 1, len(lista)):
+            if lista[j] < lista[pos_menor]:
+                pos_menor = j
+        if pos_menor != i:
+            lista[i], lista[pos_menor] = lista[pos_menor], lista[i]
+            houve_troca = True
+            print(f'Troca: {lista[i]} <-> {lista[pos_menor]}')
+        iteracoes += 1
+        print(f'Iteração {iteracoes}: {lista}')
+        if not houve_troca:
+            print("Nenhuma troca realizada, a lista já está ordenada. Encerrando.")
+            break
+    return iteracoes
